a10085d425072692d08bbb0ab8dd1234781690ed
Arnaut <arnautscholten1996@gmail.com>
2021-06-04 14:08:13 +0200

    Add option to view client data

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using CareKickoff.Services;
+using CareKickoff.Models;
+
+namespace CareKickoff.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult Index(EmployeeModel employee)
+        {
+            if (employee.IsAuthorized)
+            {
+                ClientService clientService = new ClientService();
+                ViewModel model = new ViewModel();
+                model.Client = clientService.GetClientFromId(employee.SelectedClientId);
+                model.Careplans = clientService.GetCareplansFromClient(employee.SelectedClientId);
+
+                return View("ClientData", model);
+            }
+            else
+            {
+                return View("../Login/LoginFailure", employee);
+            }
+        }
+
+
+        
+    }
+
+
+    
+}
---
+++ Services/ClientService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CareKickoff.Models;
+using Newtonsoft.Json;
+
+namespace CareKickoff.Services
+{
+    public class ClientService
+    {
+
+        ClientModel[] clients;
+        CareplanModel[] careplans;
+        
+        public ClientService()
+        {
+            string pathClients = @"e:\Projects\CareKickoff\Data\clients.json";
+            clients = JsonConvert.DeserializeObject<ClientModel[]>(File.ReadAllText(pathClients));
+
+            string pathCareplans = @"e:\Projects\CareKickoff\Data\careplans.json";
+            careplans = JsonConvert.DeserializeObject<CareplanModel[]>(File.ReadAllText(pathCareplans));
+        }
+
+        public ClientModel GetClientFromId(int id)
+        {
+            return clients.FirstOrDefault(x => x.NativeId == id);
+        }
+
+        public IEnumerable<CareplanModel> GetCareplansFromClient(int id)
+        {
+            return careplans.Where(x => x.ClientId == id);
+        }
+    }
+}
