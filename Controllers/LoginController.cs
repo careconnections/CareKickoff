7eaa6a2c0f95413d048d10ebc44db785b0d6d6d5
Arnaut <arnautscholten1996@gmail.com>
2021-06-04 14:07:06 +0200

    Add login logic

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CareKickoff.Models;
+using CareKickoff.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareKickoff.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(EmployeeModel employeeCheck)
+        {
+            EmployeeService employeeService = new EmployeeService();
+            EmployeeModel employeeData = employeeService.IsValid(employeeCheck);
+            if (employeeData != null)
+            {
+                employeeData.IsAuthorized = true;
+                return View("LoginSuccess", employeeData);
+            } else
+            {
+                return View("LoginFailure", employeeCheck);
+            }
+        }
+    }
+}
---
+++ Services/EmployeeService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CareKickoff.Models;
+using Newtonsoft.Json;
+
+namespace CareKickoff.Services
+{     
+    public class EmployeeService
+    {
+        private EmployeeModel[] employees;
+
+        public EmployeeService()
+        {
+            string path = @"e:\Projects\CareKickoff\Data\employees.json";
+            employees = JsonConvert.DeserializeObject<EmployeeModel[]>(File.ReadAllText(path));
+        }
+
+        public EmployeeModel IsValid(EmployeeModel employee)
+        {
+            return employees.FirstOrDefault(x => x.Name == employee.Name && x.Password == employee.Password);
+        }
+    }
+}
