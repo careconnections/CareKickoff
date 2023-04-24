7d5b6a00185e22e3b5fde8c77f882b78f615b2b4
Brouwer, R (Rik) <Rik.Brouwer@rabobank.com>
2023-04-24 11:19:03 +0200

    Create client overview with mock data

---
+++ CareConnections.App/CareConnections.App/Models/MockDataService.cs
@@ -0,0 +1,40 @@
+﻿using CareConnections.Shared.Domain;
+
+namespace CareConnections.App.Models
+{
+    public class MockDataService
+    {
+        private static List<Client>? _clients = default!;
+
+        public static List<Client> Clients
+        {
+            get
+            {
+                _clients ??= InitializeMockClients();
+
+                return _clients;
+            }
+        }
+
+        private static List<Client> InitializeMockClients()
+        {
+            var c1 = new Client
+            {
+                FirstName = "Test",
+                LastName = "Tester",
+                BirthDate = new DateTime(2023, 4, 24),
+                Gender = Gender.Female,
+                NativeId = "1"
+            };
+            var c2 = new Client
+            {
+                FirstName = "Rik",
+                LastName = "Brouwer",
+                BirthDate = new DateTime(1988, 5, 8),
+                Gender = Gender.Male,
+                NativeId = "2"
+            };
+            return new List<Client>() { c1, c2 };
+        }
+    }
+}
---
+++ CareConnections.App/CareConnections.App/Pages/ClientOverview.razor.cs
@@ -0,0 +1,17 @@
+﻿using CareConnections.App.Models;
+using CareConnections.Shared.Domain;
+
+namespace CareConnections.App.Pages
+{
+    public partial class ClientOverview
+    {
+        public IList<Client>? Clients { get; set; } = default!;
+
+        private readonly string Title = "Client Overview";
+
+        protected override void OnInitialized()
+        {
+            Clients = MockDataService.Clients;
+        }
+    }
+}
